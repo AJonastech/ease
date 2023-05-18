707f0793e769e85fd480d32576a95fd0d4af6f83
AJonastech <113301012+AJonastech@users.noreply.github.com>
2023-05-18 13:09:23 +0100

    mn

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,12 @@
 /* eslint-disable react/style-prop-object */
 import React, {useState} from "react";
-import { AiOutlineArrowRight } from "react-icons/ai";
 import main from "../assets/Images/main.png";
 import hero1 from "../assets/Images/hero1.png";
 import hero2 from "../assets/Images/hero2.png";
 import hero3 from "../assets/Images/hero3.png"
 import HeroImg from "./HeroImg";
 import { motion } from "framer-motion";
+
 function Hero() {
     const textData = ["Zero Fees", "Free Wires", "Unlimited Rewards", "No Credit Check"]
  const [count, setCount] = useState(0);
@@ -34,7 +34,7 @@ function Hero() {
           business. Open an account in under 3 minutes.
         </p>
         <button className="bg-primary text-white px-4 py-2 ">
-          Get Started <AiOutlineArrowRight className="inline-block" />
+          Get Started
         </button>
         <p>
           {" "}
