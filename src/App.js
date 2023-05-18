84e583def53384f2894e09f16592535e3dd10c5f
Tachy-22 <entekumejeffrey@gmail.com>
2023-05-18 16:24:54 +0100

    .

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import bgVideo from "./assets/videos/-9e7d-436d-8c86-9a177e4a782d.mp4";
 
 function App() {
   return (
-    <main className="relative flex flex-col h-screen  sm:min-h-[170vh] min-h-[130vh]  ">
+    <main className="relative flex flex-col h-screen   min-h-screen  ">
       <video
         src={bgVideo}
         type="video/mp4"
@@ -20,9 +20,8 @@ function App() {
       <section className="absolute h-fit top-0 bottom-0 left-0 right-0">
         <Nav />
         <Hero />
+        <Footer />
       </section>
-
-      <Footer />
     </main>
   );
 }
